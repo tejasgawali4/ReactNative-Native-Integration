352192659b57a4dde8d52d6fe79d49aeb0fdaff7
Tejas Gawali <tejas.gawali@synechron.com>
2022-12-09 16:09:36 +0530

    inital React Native Project added

---
+++ App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import {
+  Alert,
+  Button,
+  Pressable,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const onClick = () => {
+    console.log('clicked..');
+  }
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? "light-content" : "dark-content"}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={[styles.scroll, backgroundStyle]}
+      >
+        <View style={styles.container}>
+          <Text> This is React Native Screen 1</Text>
+          <Button onPress={onClick} title="Navigate to Native"/>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 100,
+  },
+  scroll: {
+    height: '100%',
+    margin: 20,
+  }
+});
+
+export default App;
